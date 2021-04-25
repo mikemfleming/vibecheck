95b6c975f92f11f2ab05c01602e11a763dd31207
sphynx <sphynx@sphynxs-MacBook-Pro.local>
2021-04-24 23:54:56 -0500

    wip(darkmode): begins dark mode theming

--- src/App.js
+++ src/App.js
@@ -6,14 +6,19 @@ import {
   useParams
 } from "react-router-dom";
 
+import  {useDarkMode} from "./components/useDarkMode"
+import { ThemeProvider } from "styled-components";
+import { GlobalStyles } from "./components/globalStyles";
+import { lightTheme, darkTheme } from "./components/Theme"
+import Toggle from "./components/Toggler"
 // <Link to="/account?name=modus-create">Modus Create</Link>
 export default function App() {
   return (
     <Router>
-        <Switch>
-          <Route path="/u/:username" children={<User />} />
-          <Route path="/v/:vibename" children={<Vibe />} />
-        </Switch>
+      <Switch>
+        <Route path="/u/:username" children={<User />} />
+        <Route path="/v/:vibename" children={<Vibe />} />
+      </Switch>
     </Router>
   );
 }
@@ -22,11 +27,19 @@ function User() {
   // We can use the `useParams` hook here to access
   // the dynamic pieces of the URL.
   let { username } = useParams();
+  const [theme, themeToggler] = useDarkMode()
+
+  const themeMode = theme === 'light' ? lightTheme : darkTheme;
 
   return (
-    <div>
-      <h3>username: {username}</h3>
-    </div>
+    <ThemeProvider theme={themeMode}>
+      <GlobalStyles/>
+      <div>
+        <Toggle theme={theme} toggleTheme={themeToggler} />
+        <h3>username: {username}</h3>
+      </div>
+    </ThemeProvider>
+
   );
 }
 
